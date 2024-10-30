45e4365cc42785d3c9df78bce448a06ec6a6dc17
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-10-30 18:47:03 +0500

    test changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,29 @@
 import React from "react";
-import  {Grid}  from "@mui/material";
-import Sidebar from "./Components/Sidebar/Sidebar";
-import Header from "./Components/Header/Header";
-import OverviewCards from "./Components/OverviewCards/OverviewCards";
-import ConversationsChart from "./Components/ConversationChart/ConversationChart";
-import OperatorsList from "./Components/OperatorsList.tsx/OperatorsList";
-import ConversationsTable from "./Components/ConversationTable/ConversationTable";
-import menuItems from "./data/menuItems";
-import overviewCardsData from "./data/overviewCardData";
-import operatorsData from "./data/operatorsData";
-import conversationsData from "./data/conversationData";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Dashboard from "./pages/Dashboard";
 
-const Dashboard = () => {
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2C6B56",
+    },
+    background: {
+      default: "#f4f5f7",
+    },
+  },
+  typography: {
+    fontFamily: "Arial, sans-serif",
+  },
+});
+
+const App: React.FC = () => {
   return (
-    <Grid container style={{ height: "100vh" }}>
-      
-      <Grid item xs={2} style={{ backgroundColor: "#2C6B56" }}>
-        <Sidebar menuItems={menuItems} />
-      </Grid>
-      <Grid item xs={10} style={{ padding: "20px" }}>
-        <Header name="User Name" phone="+1234567890" />
-        <OverviewCards cardsData={overviewCardsData} />
-        <Grid container spacing={2} style={{ marginTop: "20px" }}>
-          <Grid item xs={6}>
-            <ConversationsChart
-              labels={["Sent", "Delivered", "Read", "Replies"]}
-              data={[80, 60, 40, 20]}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <OperatorsList operators={operatorsData} />
-          </Grid>
-        </Grid>
-        <ConversationsTable conversations={conversationsData} />
-      </Grid>
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
   );
 };
 
-export default Dashboard;
+export default App;
