b1b1a5229691987685b1699e0b88eb42298786fe
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-03 18:29:04 +0500

    dashboard tsx edited

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@mui/material";
 import Sidebar from "../Components/Sidebar/Sidebar";
 import Header from "../Components/Header/Header";
@@ -6,15 +6,34 @@ import OverviewCards from "../Components/OverviewCards/OverviewCards";
 import ConversationsChart from "../Components/ConversationChart/ConversationChart";
 import OperatorsList from "../Components/OperatorsList.tsx/OperatorsList";
 import ConversationsTable from "../Components/ConversationTable/ConversationTable";
+import AddOperatorModal from "../Components/addOperator/AddOperatorModal";
 import menuItems from "../data/menuItems";
 import overviewCardsData from "../data/overviewCardData";
-import operatorsData from "../data/operatorsData";
+import initialOperatorsData from "../data/operatorsData";
 import conversationsData from "../data/conversationData";
 
 const Dashboard: React.FC = () => {
+  const [operators, setOperators] = useState(initialOperatorsData); 
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+ 
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  
+  const handleAddOperatorSubmit = (newOperator: { name: string; email: string; phoneNumber: string; role: string }) => {
+    setOperators((prevOperators) => [...prevOperators, newOperator]); 
+    handleCloseModal(); 
+  };
+
   return (
     <Grid container style={{ height: "100vh" }}>
-      
       <Grid item xs={2} style={{ backgroundColor: "#2C6B56" }}>
         <Sidebar menuItems={menuItems} />
       </Grid>
@@ -29,11 +48,18 @@ const Dashboard: React.FC = () => {
             />
           </Grid>
           <Grid item xs={6}>
-            <OperatorsList operators={operatorsData} />
+            <OperatorsList operators={operators} onAddOperator={handleOpenModal} />
           </Grid>
         </Grid>
 
         <ConversationsTable conversations={conversationsData} />
+
+        
+        <AddOperatorModal
+          open={isModalOpen}
+          onClose={handleCloseModal}
+          onSubmit={handleAddOperatorSubmit} 
+        />
       </Grid>
     </Grid>
   );
